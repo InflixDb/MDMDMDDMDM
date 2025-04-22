952b988cb2bc1859222d920388f15a4da92eba22
InflixDb <kartikbotam@yahoo.com>
2025-04-22 09:41:26 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+MONGO_URI = os.getenv("MONGO_URI")
+OWNER_ID = int(os.getenv("OWNER_ID"))
