b619d06704b7e8e315328ebbd1ffc12660e91abb
InflixDb <kartikbotam@yahoo.com>
2025-04-22 09:58:14 +0530

    Delete handlers/invite.py

--- handlers/invite.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, timedelta
-
-async def generate_invite(client, channel_id):
-    return await client.create_chat_invite_link(
-        chat_id=channel_id,
-        expire_date=datetime.utcnow() + timedelta(minutes=5),
-        creates_join_request=True
-    )
